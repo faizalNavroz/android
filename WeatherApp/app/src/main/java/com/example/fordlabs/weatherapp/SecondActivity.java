bd21de58042f4cb10d59a401807e2316b9737213
faizal <sfaizalfarhaan@gmail.com>
2019-07-04 16:29:04 +0530

    weather app- databinding changes

--- WeatherApp/app/src/main/java/com/example/fordlabs/weatherapp/SecondActivity.java
+++ WeatherApp/app/src/main/java/com/example/fordlabs/weatherapp/SecondActivity.java
@@ -1,9 +1,15 @@
 package com.example.fordlabs.weatherapp;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+
+import com.example.fordlabs.weatherapp.databinding.ViewpagerItemBinding2;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,47 +18,70 @@ import retrofit2.Response;
 public class SecondActivity extends AppCompatActivity {
 
     //ApiClient apiClient;
+    ArrayList<WeatherResponse> weatherResponseObjList = new ArrayList<WeatherResponse>();
+
+    String[] rank;
+
     ApiInterface apiInterface;
     TextView base;
     TextView timezone;
     TextView name;
 
+    ViewPager viewPager ;
+    MyPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_main);
+        setContentView(R.layout.viewpager_main);
 
-        //get the fields to populate data
-        base = findViewById(R.id.base);
-        timezone = findViewById(R.id.timezone);
-        name = findViewById(R.id.name);
-
-
-        Bundle receivedBundle = getIntent().getExtras();
-        String cityName = receivedBundle.get("cityName").toString();
+        rank = new String[]{"1", "2"}; //no of view pager instances needed.
 
         apiInterface = ApiClient.getRetrofit().create(ApiInterface.class);
 
-        final Call<WeatherResponse> weatherResponseCall = apiInterface.getWeatherResponse(cityName);
+        String [] cityNames ={"London,uk","Chennai"};
 
-        weatherResponseCall.enqueue(new Callback<WeatherResponse>() {
-            @Override
-            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+        for(String city : cityNames){
 
-                Log.d("msg************",response.body().toString());
-                WeatherResponse weatherResponseObj = response.body();
-                base.setText(weatherResponseObj.getName());
-                timezone.setText(String.valueOf(weatherResponseObj.getTimezone()));
-                name.setText(weatherResponseObj.getName());
+            final Call<WeatherResponse> weatherResponseCall = apiInterface.getWeatherResponse(city);
 
-            }
-            @Override
-            public void onFailure(Call<WeatherResponse> call, Throwable t) {
-                Log.d("msg","error");
-            }
-        });
+            weatherResponseCall.enqueue(new Callback<WeatherResponse>() {
+                @Override
+                public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                    Log.d("msg************", response.body().toString());
+                    if (response.isSuccessful()) {
+                        setResouceForViewPager(response);
+
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                    Log.d("msg", "error");
+                }
+            });
+
+        }
+    }
+
+
+       public  void setResouceForViewPager(Response<WeatherResponse> response) {
+           if(null != response){
+               weatherResponseObjList.add(response.body());
+           }
+
+           if(weatherResponseObjList.size() == 2){
+               viewPager = (ViewPager)findViewById(R.id.pager);
+               // Pass results to ViewPagerAdapter Class
+               adapter = new MyPagerAdapter(SecondActivity.this, rank, weatherResponseObjList);
+               // Binds the Adapter to the ViewPager
+               viewPager.setAdapter(adapter);
+           }
+
+        }
 
     }
 
-}
+
